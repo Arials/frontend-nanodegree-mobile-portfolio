c4c58d3bf8db774e3cd14d53483ef3e3ba3dcc95
Adriel <my@email.com>
2016-03-23 17:32:44 +0100

    Add npm config and Grunt config for make a dist directory

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		cssmin: {
+			target: {
+			  	files: [{
+			    	expand: true,
+			    	cwd: 'src/css',
+			    	src: ['*.css', '!*.min.css'],
+			    	dest: 'dist/css',
+			    	ext: '.css'
+			  	}]
+			}
+		},
+
+		copy: {
+			main: {
+			    files: [
+			      {expand: true, cwd: 'src/', src: ['**'], dest: 'dist/'},
+			    ],
+			},
+		},
+
+		htmlmin: {
+		    dist: {                                      // Target
+		      options: {                                 // Target options
+		        removeComments: true,
+		        collapseWhitespace: true
+		      },
+		      files: {                                   // Dictionary of files
+		        'dist/index.html': 'src/index.html',     // 'destination': 'source'
+		        'dist/project-2048.html': 'src/project-2048.html',
+		        'dist/project-mobile.html': 'src/project-mobile.html',
+		        'dist/project-webperf.html': 'src/project-webperf.html',
+		        'dist/views/pizza.html': 'src/views/pizza.html'
+		      }
+		    },
+		},
+
+		jshint: {
+	    	files: ['Gruntfile.js', 'src/**/*.js'],
+	    		options: {
+	        		globals: {
+	          		jQuery: true
+	        	}
+	      	}
+	    },
+
+	    watch: {
+	      files: ['<%= jshint.files %>'],
+	      tasks: ['jshint']
+	    },
+
+	    uglify: {
+		    my_target: {
+		      files: {
+		        'dist/js/perfmatters.js': ['src/js/perfmatters.js', 'src/views/js/main.js'],
+		        'dist/views/js/main.js': ['src//views/js/main.js']
+		      }
+		    }
+		}
+	});
+
+
+	// Register all minify and tasks
+  	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['copy', 'cssmin', 'htmlmin', 'uglify']);
+	grunt.registerTask('jshint', ['jshint']);
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# Programs needed
+node (for use npm).
+Download and install from https://nodejs.org/en/download/
+
+Follow this instructions to configure the programs:
+1. install node
+2. install ngrok: npm install ngrok -save-dev
+3. install grunt: npm install -g grunt-cli
+4. Install utility for make a Grunt.js: npm install -g grunt-init
+5. npm install grunt-pagespeed
+6. npm install load-grunt-tasks
+http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/
